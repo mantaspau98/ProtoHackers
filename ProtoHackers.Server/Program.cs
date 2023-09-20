796837ca61049e60947ac31dfca2e68c9b1e185d
Mantas Paukstaitis <mantas.paukstaitis@stormgeo.com>
2023-09-20 22:32:28 +0300

    solution to first problem

---
+++ ProtoHackers.Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 8080);
+Socket socket = new Socket(localEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+socket.Bind(localEndPoint);
+socket.Listen(5);
+        
+while (true)
+{
+    Socket handler = socket.Accept();
+            
+    while (true)
+    {
+        var buffer = new byte[1024];
+        int bytesReceived = handler.Receive(buffer);
+                
+        if (bytesReceived > 0)
+        {
+            ArraySegment<byte> arraySlice = new ArraySegment<byte>(buffer, 0, bytesReceived);
+            handler.Send(arraySlice);
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    handler.Shutdown(SocketShutdown.Send);
+    handler.Close();
+}
